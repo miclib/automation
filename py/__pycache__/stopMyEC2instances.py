855a5c98b9718236e93a9e0300fa19dd7160c72e
Michael Liberman <miclibdb@gmail.com>
2020-11-08 17:51:30 +0000

    changes

---
+++ py/__pycache__/stopMyEC2instances.py
@@ -0,0 +1,18 @@
+import sys 
+import boto3
+
+from botocore.exceptions import ClientError
+
+
+
+ec2 = boto3.client('ec2',region_name='eu-west-2')
+
+isId='i-06dcd0cf1b5d639c5'
+
+try: 
+        response = ec2.stop_instances(InstanceIds=[isId],DryRun=False)
+        print(response)
+
+except ClientError as e:
+        print(e)
+    
